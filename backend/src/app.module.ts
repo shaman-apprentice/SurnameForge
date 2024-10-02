023233c1e355045f18919e1ffb5328df6ce46745
shaman-apprentice <Torsten.Knauf@gmx.de>
2024-10-02 20:57:38 +0200

    Implement PoC for static file server by NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { WordCloudModule } from './wordCloud/wordCloud.module';
 import { SurveyModule } from './survey/survey.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from "path";
 
 @Module({
   imports: [
     WordCloudModule,
     SurveyModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../frontend/dist/surname-forge/browser'),
+      exclude: [ '/api/(.*)' ],
+    }),
   ],
 })
 export class AppModule {}
