abcee2ac3d0386bbc2f93284aa49595131b519ab
shaman-apprentice <torsten@severing.de>
2025-01-21 22:01:20 +0100

    fix: Allow multi language routing to survey2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,15 +7,18 @@ import { environment } from '../environments/environment';
 import { Survey2Page } from './routes/survey2/survey2.page';
 
 export const routes: Routes = environment.showOnlySurvey
-  ? [ { path: "survey", component: SurveyPage } ]
+  ? [ 
+    { path: "survey2", component: Survey2Page },
+    { path: "survey", component: SurveyPage },
+  ]
   : [
     { path: "about", component: AboutPage },
     { path: "surname-forge", component: SurnameForgePage },
-    { path: "survey", component: SurveyPage },
     { path: "survey2", component: Survey2Page },
+    { path: "survey", component: SurveyPage },
   ];
 
 routes.push(...[
   { path: "impressum", component: ImpressumPage },
-  { path: "**",   redirectTo: "/survey" },
+  { path: "**",   redirectTo: "/survey2" },
 ]);
